ce410dc6e1e33a07917c44fc9a910dc7f9721114
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-04-21 12:10:02 +0200

    Add: rotta corretta per visualizzazione home index giusta e predisposizione index Foto

--- IlMioFotalbum/IlMioFotalbum/Controllers/FotoController.cs
+++ IlMioFotalbum/IlMioFotalbum/Controllers/FotoController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IlMioFotalbum.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IlMioFotalbum.Controllers
 {
     public class FotoController : Controller
     {
+
         public IActionResult Index()
+        {
+            using var ctx = new FotoContext();
+
+            var foto = ctx.Fotos.ToArray();
+
+            return View(foto);
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
